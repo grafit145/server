4113b5a1b63864a10aff832527ba7fb3e820ca18
Anton Boriskin <antbor23@gmail.com>
2021-07-26 18:50:57 +0300

    First commit

---
+++ Client.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+
+        String host = "localhost";
+        int port = 8282;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            out.println("Client message");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        int port = 8282;
+        while (true) {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("New connection accepted");
+
+            final String message = in.readLine();
+            System.out.println(String.format("%s; port is %d", message, clientSocket.getPort()));
+            serverSocket.close();
+
+            break;
+        }
+    }
+}
